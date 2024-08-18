496f8e058c380f298c341dff1f27cd18aef9427e
michalmonday <michalmonday17@gmail.com>
2024-08-18 22:51:36 +0100

    vcd_to_datapoints.py args

--- builds/RV64ACDFIMSUxCHERI_Flute_bluesim_debug/vcd_to_datapoints.py
+++ builds/RV64ACDFIMSUxCHERI_Flute_bluesim_debug/vcd_to_datapoints.py
@@ -1,7 +1,14 @@
-import pandas as pd
-
-# read vcd wave file
 import pyvcdr
+import sys
+import argparse
+
+parser = argparse.ArgumentParser(description='Reads VCD file and stores the states of signals of interest in a CSV file.')
+#positional arg
+parser.add_argument('vcd_file', type=str, help='VCD file to read')
+#optional args
+parser.add_argument('--output', '-o', type=str, default='vcd_to_datapoints_result.csv', help='Output CSV file')
+args = parser.parse_args()
+
 
 def read_signals_of_interest(f_name, signals_of_interest):
     ''' Reads .vcd waveform file and returns "steps",
@@ -58,15 +65,17 @@ def store_states_in_csv(states, f_name, header=None):
         for state in states:
             f.write(','.join([str(s) for s in state]) + '\n')
 
+
 signals_of_interest = [
     'imem_rg_pc', 
     'rg_written_reg_name', # GPR address 
     'rg_written_reg_value'
 ]
-pcs_steps, gpr_addresses_steps, gpr_values_steps = read_signals_of_interest('sim2.vcd', signals_of_interest)
+
+pcs_steps, gpr_addresses_steps, gpr_values_steps = read_signals_of_interest(args.vcd_file, signals_of_interest)
 states = sync_other_steps(pcs_steps, [gpr_addresses_steps, gpr_values_steps])
 
-store_states_in_csv(states, 'check_halting_integrity_states.csv', header='pc,rg_addr,rg_val')
+store_states_in_csv(states, args.output, header=','.join(signals_of_interest))
 
 # pcs_steps = [(0, 0x8000000), (100, 0x8000004), (300, 0x8000008), (400, 0x800000c)] 
 # all_other_steps_list = [
@@ -75,167 +84,4 @@ store_states_in_csv(states, 'check_halting_integrity_states.csv', header='pc,rg_
 # ]
 # states = get_program_counter_states(pcs_steps, all_other_steps_list)
 
-print(states)
-raise SystemExit
-
-
-
-
-# # for sig_name, sig in signals_of_interest.items():
-# #     if sig is None:
-# #         print(f'Signal {sig_name} not found')
-# #         continue
-# #     if sig_name == 'rg_cycle':
-# #         continue
-# #     print(f'Signal {sig_name} found')
-# #     for i, (time, value) in enumerate(sig.steps):
-# #         print(f'{i}: {time} {value:X}')
-
-# raise SystemExit
-
-# # import json
-# # import sys
-# # from pyDigitalWaveTools.vcd.parser import VcdParser
-
-# # with open('sim2.vcd') as vcd_file:
-# #     vcd = VcdParser()
-# #     vcd.parse(vcd_file)
-# #     data = vcd.scope.toJson()
-# #     import pdb; pdb.set_trace()
-# #     print(json.dumps(data, indent=4, sort_keys=True))
-
-# # def read_signal_values_from_vcd_file(signals=None, f_name='vlt_dump.vcd'):
-# #     """Reads the signal values from a VCD file and returns a dictionary
-# #     with the signal names as keys and a list of tuples (time, value) as
-# #     values.
-# #     """
-# #     signal_values = {}
-# #     time = 0
-# #     with open(f_name, 'r') as f:
-# #         for line in f:
-# #             line = line.strip()
-# #             if line.startswith('$var'):
-# #                 values = line.split()
-# #                 vcd_signal_name = values[3]
-# #                 verilog_signal_name = values[4]
-# #                 if verilog_signal_name in signals:
-# #                     signal_values[vcd_signal_name] = {'name': verilog_signal_name, 'values': []}
-# #             elif line.startswith('#'):
-# #                 time = int(line[1:])
-# #             elif line.startswith('b'):
-# #                 value, vcd_signal_name = line.split()
-# #                 print(f'{time} {value} {vcd_signal_name}')
-# #                 value = int(value[1:], 2)
-# #                 if vcd_signal_name in signal_values:
-# #                     signal_values[vcd_signal_name]['values'].append((time, value))
-# #                     # print(line)
-# #                     # import pdb; pdb.set_trace()
-# #     return signal_values
-
-# values = read_signal_values_from_vcd_file(signals=['rg_cycle'], f_name='sim2.vcd')#  signals = ['send_performance_events_evts'])
-
-# import pdb; pdb.set_trace()
-
-# all_events = [[] for _ in range(115)]
-# for vcd_signal_name, values in values.items():
-#     print(values['name'])
-#     for time, value in values['values']:
-#         mask = (1 << 64) - 1
-#         for i in range(115):
-#             all_events[i].append(value & mask)
-#             value >>= 64
-
-# df = pd.DataFrame(all_events, index=event_names).T
-# for col in df:
-#     print(df[col].value_counts())
-
-# # these events are counted (and later collected/traced) by continuous monitoring system
-# used_events = [
-#     'Core__BRANCH',
-#     'Core__JAL',
-#     'Core__LOAD',
-#     'Core__STORE',
-#     'L1I__LD',
-#     'L1D__LD',
-#     'TGC__WRITE',
-#     'TGC__READ'
-# ]
-
-# # these events were observed to be not zero when running a short program
-# # + Core__TRAP and Core__INTERRUPT added
-# selected_events = [
-#     'Core__TRAP', # added manually
-#     'Core__BRANCH',
-#     'Core__JAL',
-#     'Core__JALR',
-#     'Core__AUIPC',
-#     'Core__LOAD',
-#     'Core__STORE',
-#     'Core__SERIAL_SHIFT',
-#     'Core__LOAD_WAIT',
-#     'Core__STORE_WAIT',
-#     'Core__F_BUSY_NO_CONSUME',
-#     'Core__1_BUSY_NO_CONSUME',
-#     'Core__2_BUSY_NO_CONSUME',
-#     'Core__INTERRUPT', # added manually
-#     'L1I__LD',
-#     'L1I__LD_MISS',
-#     'L1I__LD_MISS_LAT',
-#     'L1I__TLB',
-#     'L1D__LD',
-#     'L1D__LD_MISS',
-#     'L1D__LD_MISS_LAT',
-#     'L1D__ST',
-#     'L1D__TLB',
-#     'TGC__WRITE',
-#     'TGC__WRITE_MISS',
-#     'TGC__READ',
-#     'TGC__READ_MISS',
-#     'TGC__EVICT',
-#     'TGC__SET_TAG_WRITE',
-#     'TGC__SET_TAG_READ',
-#     'AXI4_Slave__AW_FLIT',
-#     'AXI4_Slave__W_FLIT',
-#     'AXI4_Slave__W_FLIT_FINAL',
-#     'AXI4_Slave__B_FLIT',
-#     'AXI4_Slave__AR_FLIT',
-#     'AXI4_Slave__R_FLIT',
-#     'AXI4_Slave__R_FLIT_FINAL',
-#     'AXI4_Master__AW_FLIT',
-#     'AXI4_Master__W_FLIT',
-#     'AXI4_Master__W_FLIT_FINAL',
-#     'AXI4_Master__B_FLIT',
-#     'AXI4_Master__AR_FLIT',
-#     'AXI4_Master__R_FLIT',
-#     'AXI4_Master__R_FLIT_FINAL'
-#     ]
-
-
-
-# used_events_indices = [event_names.index(e) for e in used_events]
-# selected_events_indices = [event_names.index(e) for e in selected_events]
-
-# with open('performance_event_names_used.csv', 'w') as f:
-#     f.write('orig_index,new_index,event_name\n')
-#     for new_index, (orig_index, e) in enumerate(zip(used_events_indices, used_events)):
-#         if new_index != 0:
-#             f.write('\n')
-#         f.write(f'{orig_index},{new_index},{e}')
-
-# with open('performance_event_names_selected.csv', 'w') as f:
-#     f.write('orig_index,new_index,event_name\n')
-#     for new_index, (orig_index, e) in enumerate(zip(selected_events_indices, selected_events)):
-#         if new_index != 0:
-#             f.write('\n')
-#         f.write(f'{orig_index},{new_index},{e}')
-
-# with open('performance_event_names_selected_template.txt', 'w') as f:
-#     f.write('This file contains lines that can be pasted into CPU.bsv\n')
-#     for new_index, (orig_index, e) in enumerate(zip(selected_events_indices, selected_events)):
-#         if new_index != 0:
-#             f.write('\n')
-#         f.write(f'performance_events_local[{new_index}] = events[{orig_index}][0]; // {e}')
-
-#     f.write('\n\n')
-#     f.write(f'Selected events counts: {len(selected_events_indices)}')
-
+# print(states)
